f57aa7abe9828f15e12eabc033067af78ff960ff
Abdullah <amaatouq@gmail.com>
2018-06-16 05:47:33 -0400

    update server prints to track the progress of the game

--- imports/experiment/server/game/callbacks.js
+++ imports/experiment/server/game/callbacks.js
@@ -1,4 +1,6 @@
-export const onRoundStart = (game, round, players) => {};
+export const onRoundStart = (game, round, players) => {
+  console.log("round",round.index+1,"For game ", game._id, "has started");
+};
 
 export const onStageEnd = (game, round, stage, players) => {};
 
@@ -17,7 +19,7 @@ export const onRoundEnd = (game, round, players) => {
 
 export const onGameEnd = (game, players) => {
   const conversionRate = 1 / 18.0;
-  console.log("The game has ended for ");
+  console.log("The game has ended for ", game._id);
   players.forEach(player => {
     player.set("bonus", Math.round(player.get("score") * conversionRate) || 0);
   });
